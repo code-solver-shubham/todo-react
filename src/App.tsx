ab2cb07ef7cd8302e74db498376d65148ba7300a
Codeby-shubham <sr9835346791@gmail.com>
2025-11-07 18:38:39 +0530

    Create App component to render Todo section

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Todo } from "./Project/Todo/Todo";
+
+const App = () => {
+  return (
+    <>
+      <section>
+        <Todo />
+      </section>
+    </>
+  );
+};
+
+export default App;
